2072fa66cd9dc5b6c205674ca4ac535fbc230c14
Vicentia Pius Irabor <vicentiapeace@gmail.com>
2022-08-17 19:04:58 -0400

    first commit

---
+++ contracts/sittoken.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SIT is ERC20 {
+    uint public constant maxTotalSupply = 100000 *10 ** 18;
+
+    constructor() ERC20("SIT-TOKEN", "SIT") {}
+     
+     function mint (uint _amount) internal{
+            _mint  (msg.sender, (_amount*10**18));
+    }
+}
---
+++ scripts/deploy.ts
@@ -0,0 +1,23 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  // const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  // const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+
+  // const lockedAmount = ethers.utils.parseEther("1");
+
+  const DEGT = await ethers.getContractFactory("DEGT");
+  const degt = await DEGT.deploy();
+
+  await degt.deployed();
+
+  console.log("DEG Token deployed to: ", degt.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
